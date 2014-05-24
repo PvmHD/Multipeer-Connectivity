6bd597383f4ad51a6f2b8b6b3fdefc73ae860840
Yan Gorshtenin <support@netlodge.net>
2014-05-23 17:28:06 -0700

    Commit #1

---
+++ MultipeerConnectivityViewController.h
@@ -0,0 +1,12 @@
+/
+// MultipeerConnectivityViewController.h
+//
+// Created by Yan Gorshtenin on 5/23/14
+// Copyright (c) 2014 Yan Gorshtenin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultipeerConnectivityViewController : UIViewController
+
+@end
